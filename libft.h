5420b774183975c9f354542a2ad93e7307559fb7
Juho Kangas <jkangas@student.hive.fi>
2021-11-03 14:17:05 +0200

    add first stuff

---
+++ libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 12:56:10 by jkangas           #+#    #+#             */
+/*   Updated: 2021/11/03 13:37:21 by jkangas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+size_t	ft_strlen(const char *s);
+
+#endif
