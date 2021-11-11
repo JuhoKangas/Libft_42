bef377f5b51f7311be94ccb8b2be13de437305c5
Juho Kangas <jkangas@student.hive.fi>
2021-11-11 17:36:41 +0200

    add bzero and memcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 12:56:10 by jkangas           #+#    #+#             */
-/*   Updated: 2021/11/11 13:12:24 by jkangas          ###   ########.fr       */
+/*   Updated: 2021/11/11 16:11:48 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,7 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
