efbd3849b3bf1a74b98a7c751facf7ed27356ee0
Juho Kangas <jkangas@student.hive.fi>
2021-11-22 18:54:01 +0200

    add bug fix to memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jkangas <jkangas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:48:22 by jkangas           #+#    #+#             */
-/*   Updated: 2021/11/11 16:10:15 by jkangas          ###   ########.fr       */
+/*   Updated: 2021/11/22 18:53:48 by jkangas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dst && !src)
+		return (0);
 	while (i < n)
 	{
 		((char *)dst)[i] = ((char *)src)[i];
